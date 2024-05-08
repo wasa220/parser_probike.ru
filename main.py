7712b569c101e1ccfdf033c7e27dd3f7949457fb
unknown <sergejkugaev@gmail.com>
2024-05-08 17:54:19 +0300

    add logical blocks

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
+
 URL = "https://pro-bike.ru"
-response = requests.get(URL)
-soup = bs(response.text, "lxml")
 
-footer = soup.find_all("div", class_ = "main-footer_links_col")
-raw_footer_links = []
-for block in footer[4:6]:
-    raw_footer_links.append(block.find_all('a'))
-raw_footer_links = raw_footer_links[0][1:] + raw_footer_links[1][:-1]
 
-footer_links = []
-for i in raw_footer_links:
-    footer_links.append(f'{URL}{i.get('href')}')
+def req(url):
+    response = requests.get(url = url)
+    soup = bs(response.text, "lxml")
+    
+    return soup
+
+categories_links = []
+def get_categories_links():
+    soup = req(URL)
+    footer = soup.find_all("div", class_ = "main-footer_links_col")
+    raw_footer_links = []
+    for block in footer[4:6]:
+        raw_footer_links.append(block.find_all('a'))
+    raw_footer_links = raw_footer_links[0][1:] + raw_footer_links[1][:-1]
+
+    for i in raw_footer_links:
+        categories_links.append(f'{URL}{i.get('href')}')
 
 card_links = []
-for category in footer_links[:1]:
-    response = requests.get(category)
-    soup = bs(response.text, "lxml")
+def get_card_links():
+    for category in categories_links[:1]:
+        soup = req(category)
 
-    # pagination = int(soup.find('div', class_ = 'pagination').find('li', class_ = 'pagination__button-skip').nextSibling.nextSibling.text)
-    # print(pagination)
-    cards = soup.find('div', class_ = 'items__list_catalog').find_all('a')
-    for card in cards:
-        print(card.get('href'))
+        cards = soup.find('div', class_ = 'items__list_catalog').find_all('a')
+        for card in cards[:-5]:
+            card_links.append(f'{URL}{card.get('href')}')
+
+
+if __name__ == '__main__':
+    get_categories_links()
+    get_card_links()
+    print(card_links)
