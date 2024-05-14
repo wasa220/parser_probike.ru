a15d34454f143eb1c1f170bb7f1a632a39153f2c
unknown <sergejkugaev@gmail.com>
2024-05-14 13:06:56 +0300

    get product date

--- main.py
+++ main.py
@@ -33,11 +33,21 @@ def get_card_links():
             card_links.append(f'{URL}{card.get('href')}')
 
 def get_product_data():
-    for card in card_links[:1]:
+    for card in card_links[2:3]:
         soup = req(card)
         
         product_name = soup.find('h1', class_ = 'page-title').text
         product_code = str(soup.find('div', class_ = 'item__content_tags_code').find('span').next_sibling).strip()
+        product_article = soup.find("div", class_ = 'item__info_variant_artikul').find('div', class_ = 'item__info_variant_row_value').text.strip()
+        product_availability = soup.find('div', class_ = 'item__info_variant_nalichie'). find('div', class_ = 'item__info_variant_row_value').text.strip()
+        product_price_old = soup.find('div', class_ = 'item__info_variant_price_old').text.strip()
+        product_price = soup.find('div', class_ = 'item__info_variant_price_final').text.strip()
+        product_manufacturer = soup.find('span', class_ = 'warranty__manuf_title dotted').text.strip()
+
+        product_picture = soup.find('div', class_ = 'content-container content-container__mobile-sticky item__content').find('div', class_ = 'pb-viewer__element').find('img').get('srcset').split('\n')
+
+        # sale_amount = product_price_old - product_price
+        print(product_picture)
 
 
 if __name__ == '__main__':
